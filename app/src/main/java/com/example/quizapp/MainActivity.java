fb399bc9a54fc7e1a104c11fe3d77e506503bd9f
kinaseryota <ryotakinase1023@gmail.com>
2022-08-01 22:14:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView countLabel;
+    private TextView questionLabel;
+    private Button answerBtn1;
+    private Button answerBtn2;
+    private Button answerBtn3;
+    private Button answerBtn4;
+
+    private String rightAnswer;
+    private int rightAnswerCount;
+    private int quizCount = 1;
+
+    ArrayList<ArrayList<String>> quizArray = new ArrayList<>();
+
+    String[][] quizData = {
+            // {"都道府県名", "正解", "選択肢１", "選択肢２", "選択肢３"}
+            {"北海道", "札幌市", "長崎市", "福島市", "前橋市"},
+            {"青森県", "青森市", "広島市", "甲府市", "岡山市"},
+            {"岩手県", "盛岡市","大分市", "秋田市", "福岡市"},
+            {"宮城県", "仙台市", "水戸市", "岐阜市", "福井市"},
+            {"秋田県", "秋田市","横浜市", "鳥取市", "仙台市"},
+            {"山形県", "山形市","青森市", "山口市", "奈良市"},
+            {"福島県", "福島市", "盛岡市", "新宿区", "京都市"},
+            {"茨城県", "水戸市", "金沢市", "名古屋市", "奈良市"},
+            {"栃木県", "宇都宮市", "札幌市", "岡山市", "奈良市"},
+            {"群馬県", "前橋市", "福岡市", "松江市", "福井市"},
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        countLabel = findViewById(R.id.countLabel);
+        questionLabel = findViewById(R.id.questionLabel);
+        answerBtn1 = findViewById(R.id.answerBtn1);
+        answerBtn2 = findViewById(R.id.answerBtn2);
+        answerBtn3 = findViewById(R.id.answerBtn3);
+        answerBtn4 = findViewById(R.id.answerBtn4);
+
+        for (String[] quizDatum : quizData) {
+            ArrayList<String> tmpArray = new ArrayList<>();
+
+            tmpArray.add(quizDatum[0]);
+            tmpArray.add(quizDatum[1]);
+            tmpArray.add(quizDatum[2]);
+            tmpArray.add(quizDatum[3]);
+            tmpArray.add(quizDatum[4]);
+
+            quizArray.add(tmpArray);
+
+        }
+
+        showNextQuiz();
+    }
+
+    public void showNextQuiz() {
+        countLabel.setText(getString(R.string.count_label, quizCount));
+
+        Random random = new Random();
+        int randomNum = random.nextInt(quizArray.size());
+
+        ArrayList<String> quiz = quizArray.get(randomNum);
+
+        questionLabel.setText(quiz.get(0));
+
+        rightAnswer = quiz.get(1);
+
+        quiz.remove(0);
+
+        Collections.shuffle(quiz);
+
+        answerBtn1.setText(quiz.get(0));
+        answerBtn2.setText(quiz.get(1));
+        answerBtn3.setText(quiz.get(2));
+        answerBtn4.setText(quiz.get(3));
+
+        quizArray.remove(randomNum);
+
+
+    }
+}
